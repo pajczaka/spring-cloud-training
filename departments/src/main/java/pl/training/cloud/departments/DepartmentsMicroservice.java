3a8ade52604b2a7a175c7d5d9dfb96eb4c9c99dd
Łukasz Andrzejewski <lukasz.andrzejewski@inbright.pl>
2017-12-05 22:47:06 +0100

    encrypted configuration

--- departments/src/main/java/pl/training/cloud/departments/DepartmentsMicroservice.java
+++ departments/src/main/java/pl/training/cloud/departments/DepartmentsMicroservice.java
@@ -1,6 +1,6 @@
 package pl.training.cloud.departments;
 
-import rg.eeewwwwwspringframework.boot.SpringApplication;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
