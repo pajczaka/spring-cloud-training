91a437441279edfc8ee07496fb360f89410ac53f
Łukasz Andrzejewski <lukasz.andrzejewski@inbright.pl>
2017-12-05 10:13:43 +0100

    Users

--- users/src/main/java/pl/training/cloud/users/service/BalancedDepartmentsService.java
+++ users/src/main/java/pl/training/cloud/users/service/BalancedDepartmentsService.java
@@ -5,17 +5,20 @@ import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 import pl.training.cloud.users.model.Department;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @Service
 public class BalancedDepartmentsService implements DepartmentsService {
 
     private static final String DEPARTMENTS_MICROSERVICE = "departments-microservice";
-    private static final String RESOURCE_NAME = "departments";
+    private static final String RESOURCE_NAME = "/departments/";
 
     private DiscoveryClient discoveryClient;
 
@@ -24,7 +27,7 @@ public class BalancedDepartmentsService implements DepartmentsService {
         this.discoveryClient = discoveryClient;
     }
 
-    @Cacheable
+    @Cacheable(value = "departments", unless = "#result == null")
     @Override
     public Optional<Department> getDepartmentById(Long id) {
         Optional<String> serviceUri = getServiceUri(id);
@@ -35,8 +38,12 @@ public class BalancedDepartmentsService implements DepartmentsService {
     }
 
     private Optional<Department> getDepartment(String serviceUri) {
-       Department department = new RestTemplate()
-               .getForObject(serviceUri, Department.class);
+       Department department = null;
+       try {
+           department = new RestTemplate().getForObject(serviceUri, Department.class);
+       } catch (HttpClientErrorException ex) {
+           Logger.getLogger(getClass().getName()).log(Level.INFO, "### Fetching department failed");
+       }
        return Optional.ofNullable(department);
     }
 
@@ -46,8 +53,7 @@ public class BalancedDepartmentsService implements DepartmentsService {
         if (serviceInstances.isEmpty()) {
             return Optional.empty();
         }
-        return Optional.of(serviceInstances.get(0).getUri().toString()
-                + RESOURCE_NAME + departmentId);
+        return Optional.of(serviceInstances.get(0).getUri().toString() + RESOURCE_NAME + departmentId);
     }
 
 }
