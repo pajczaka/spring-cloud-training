ef5c956ab55d4af2fd21b4cba59409bb5ffb32fc
Łukasz Andrzejewski <lukasz.andrzejewski@inbright.pl>
2017-12-05 15:11:44 +0100

    Users

--- run-kafka.sh
+++ run-kafka.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run -p 2181:2181 -p 9092:9092 --env ADVERTISED_HOST=192.168.99.100 --env ADVERTISED_PORT=9092 spotify/kafka
+docker run -p 8800:2181 -p 8001:9092 --env ADVERTISED_HOST=192.168.99.100 --env ADVERTISED_PORT=9092 spotify/kafka
