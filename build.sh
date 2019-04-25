0aad2a932b9cf656cf3cc95549cf0b07c7137198
Łukasz Andrzejewski <lukasz.andrzejewski@inbright.pl>
2019-04-25 14:58:16 +0200

    module

--- build.sh
+++ build.sh
@@ -7,5 +7,6 @@ mvn clean package
 docker build -t "training/configuration-server" configuration
 docker build -t "training/discovery-server" discovery
 docker build -t "training/gateway-server" gateway
+docker build -t "training/zipkin-server" zipkin
 docker build -t "training/departments" departments
 docker build -t "training/users" users
