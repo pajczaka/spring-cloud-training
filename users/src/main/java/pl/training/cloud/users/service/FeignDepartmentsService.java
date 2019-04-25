a8b48de167aa35f8728cd79a91639aa395e51c2c
Łukasz Andrzejewski <lukasz.andrzejewski@inbright.pl>
2019-04-25 08:40:37 +0200

    module

--- users/src/main/java/pl/training/cloud/users/service/FeignDepartmentsService.java
+++ users/src/main/java/pl/training/cloud/users/service/FeignDepartmentsService.java
@@ -1,8 +1,8 @@
 package pl.training.cloud.users.service;
 
+import feign.FeignException;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
-import lombok.Value;
 import lombok.extern.java.Log;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
@@ -10,7 +10,6 @@ import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.cloud.stream.messaging.Sink;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.HttpClientErrorException;
 import pl.training.cloud.users.dto.DepartmentDto;
 
 import java.util.Optional;
@@ -33,7 +32,7 @@ public class FeignDepartmentsService implements DepartmentsService {
                 log.info("Fetching department...");
                 return Optional.of(departmentDto.getName());
             }
-        } catch (HttpClientErrorException ex) {
+        } catch (FeignException ex) {
             log.warning("Error fetching department with id:  + departmentId");
         }
         return Optional.empty();
