f88d8632a16b30e66afc949a2c1dd5eb0267fd4e
Łukasz Andrzejewski <lukasz.andrzejewski@inbright.pl>
2019-04-25 12:49:55 +0200

    module

--- configuration/run.sh
+++ configuration/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 while ! `nc -z $KAFKA_SERVER $KAFKA_PORT`;
-    do sleep $SLEEP_TIME; echo "Waiting for kafka server";done
+    do sleep $SLEEP_TIME; echo "Waiting for ${KAFKA_SERVER} server";done
 java -jar -Dspring.profiles.active=$DEFAULT_PROFILE configuration-server.jar
